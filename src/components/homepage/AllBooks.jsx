1267fa5f5127519f41208e15a06a5d86a0486d28
NureAlamShuvo <nurealamshuvo11@gmail.com>
2026-04-06 15:36:51 +0600

    added all book from json data

--- src/components/homepage/AllBooks.jsx
+++ src/components/homepage/AllBooks.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { use } from 'react';
+import BookCard from '../ui/BookCard';
+
+const booksPromise = fetch('/booksData.json').then(res => res.json());
 
 const AllBooks = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
 
+    const books = use(booksPromise);
+    console.log(books);
+
+    return (
+        <div className='container mx-auto'>
+            <h2 className='font-bold text-[#131313] text-center text-4xl mb-9'>Books</h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-6'>
+                {
+                books.map(book => {
+                    return (
+                        <BookCard key={book.bookId} book={book} />
+                    )
+                })
+            }
+            </div>
+        </div>
+    )
+}
 export default AllBooks;
